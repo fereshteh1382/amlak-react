560f7c723e80c149fd452a97d766fa6ea90a9b0c
fereshteh hosseini <hosseini.iaum@gmail.com>
2023-05-14 22:46:12 +0430

    ignore

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,9 +2,6 @@ import http from "./httpService";
 
 import config from "./config.json";
 
-/*export const getCourses = () => {
-    return http.get(`${config.localapi}/posts/allposts_json`);
-};*/
 export const registerUser = user => {
     return http.post(
         `${config.localapi}/users/handleRegister`,
