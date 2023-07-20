f202f90765f5d9fa9ecbe953cea05052e602adeb
Mahboubeh <mh.mahdizadeh@gmail.com>
2023-07-21 01:11:57 +0330

    Add gallery

--- front/src/components/home/EstateInfo.jsx
+++ front/src/components/home/EstateInfo.jsx
@@ -1,19 +1,49 @@
 import { useContext } from "react";
 import { HomeStateContext } from "../../context/HomeStateContext";
 import config from "../../services/config.json";
+import Lightbox from "yet-another-react-lightbox";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import { isEmpty } from "lodash";
 
 const EstateInfo = () =>{
     const hssContext = useContext(HomeStateContext);
     const { selectedEstateInfo  } = hssContext;
     const estatepic  = selectedEstateInfo.thumbnail1 ? `${config.localapi}/uploads/${selectedEstateInfo.thumbnail1}` : "/pics/product.jpg";
+    const  slides=[ {src: estatepic,alt: "image 1"}];
 
+    if(selectedEstateInfo.thumbnail2 && !isEmpty(selectedEstateInfo.thumbnail2)){
+        slides.push({ src: `${config.localapi}/uploads/${selectedEstateInfo.thumbnail2}` });
+    }
+    if(selectedEstateInfo.thumbnail3 && !isEmpty(selectedEstateInfo.thumbnail3)){
+        slides.push({ src: `${config.localapi}/uploads/${selectedEstateInfo.thumbnail3}` });
+    }
+    const inline = {
+        style: {
+          width: "100%",
+          maxWidth: "500px",
+          aspectRatio: "3 / 2",
+          margin: "0 auto",
+        },
+      };
 
+    
     return(
         <section className="estate-detail-section">
     	<div className="container">
     		<div className="row">
     			<div className="col-lg-6 mb-5 ftco-animate text-center">
-                    <img className="img-fluid" src={estatepic} alt="title" style={{"max-height":"300px"}} />
+                <div style={{ width: "100%", maxWidth: "900px", aspectRatio: "3 / 2" }}>
+
+                <Lightbox slides={slides} inline={inline}         
+                    plugins={[Inline, Thumbnails]}
+ />
+
+</div>
+                    {/* <img className="img-fluid" src={estatepic} alt="title" style={{"max-height":"300px"}} /> */}
     			</div>
     			<div className="col-lg-6 product-details pl-md-5 ftco-animate" style={{maxWidth:"500px"}}>
     				<h1 className="estate-detail-header">{selectedEstateInfo.title}</h1>
